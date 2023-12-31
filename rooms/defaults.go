5ef64df57a48370fd605206f016eae617ee651a7
kts of kettek <kts@kettek.net>
2023-12-31 04:36:08 -0800

    Limit movement to 1 tile

--- rooms/defaults.go
+++ rooms/defaults.go
@@ -11,7 +11,10 @@ func defaultUpdate(r *game.Room) {
 	for _, cmd := range r.PendingCommands {
 		switch c := cmd.Cmd.(type) {
 		case commands.Move:
-			cmd.Actor.SetPosition(c.X, c.Y)
+			ax, ay := cmd.Actor.Position()
+			if ax-c.X >= -1 && ax-c.X <= 1 && ay-c.Y >= -1 && ay-c.Y <= 1 {
+				cmd.Actor.SetPosition(c.X, c.Y)
+			}
 		default:
 			fmt.Println("handle", cmd.Actor, "wants to", cmd.Cmd)
 		}
