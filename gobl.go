3c22289c521fbb32a6475e447700d3e3f430752a
kts of kettek <kts@kettek.net>
2023-12-30 19:31:05 -0800

    Update gobl

--- gobl.go
+++ gobl.go
@@ -19,7 +19,7 @@ func main() {
 	Task("run").
 		Exec(runArgs...)
 	Task("watch").
-		Watch("./cmd/*/*.go", "./res/*", "./game/*.go").
+		Watch("./cmd/*/*.go", "./res/*", "./game/*.go", "./rooms/*.go", "./states/*.go").
 		Signaler(SigQuit).
 		Run("build").
 		Run("run")
